13171670cdcc921b633608c9809ceb91cbf237bb
RS_juan <sqdrsj@gmail.com>
2016-07-31 12:11:19 +0800

    主程序

--- main.php
+++ main.php
@@ -23,41 +23,40 @@ $port = $_REQUEST['port'];									//赋值
 
 if(!isset($_REQUEST['record_id']))							//是否有记录id,没有为添加域名,有则为修改域名
 {
-$content=dnspod_freeset_srv($domain,$play_domain,$port,$srv_domain,$login_token);
+	$content=dnspod_freeset_srv($domain,$play_domain,$port,$srv_domain,$login_token);
 															//连接api 获取返回的数组
-if($content['status']['code']!=1)
-{
-	$res=array('errno'=>'3','errmsg'=>$content['status']['message']);
-	echo json_encode($res);									//错误信息输出
-	return;													//结束
-}
-
-$res=array(
-'errno' =>'1',
-'errmsg'=>iconv("gb2312","UTF-8",'映射成功,域名为:'.$srv_domain.'.'.$domain.', 请记住ID,修改和删除域名需要'),
-'record_id'    =>$content['record']['id'],					//成功,信息
-
-);
-echo json_encode($res);										//输出
+	if($content['status']['code']!=1)
+	{
+		$res=array('errno'=>'3','errmsg'=>$content['status']['message']);
+		echo json_encode($res);									//错误信息输出
+		return;													//结束
+	}
+	$res=array(
+	'errno' =>'1',
+	'errmsg'=>iconv("gb2312","UTF-8",'映射成功,域名为:'.$srv_domain.'.'.$domain.', 请记住ID,修改和删除域名需要'),
+	'record_id'    =>$content['record']['id'],					//成功,信息
+	);
+	echo json_encode($res);										//输出
 }
 
 else														//修改srv域名信息
 {
-$record_id=$_REQUEST['record_id'];							//赋值记录id
-$content=dnspod_freeset_srv_remove($domain,$play_domain,$port,$srv_domain,$record_id,$login_token);
+	$record_id=$_REQUEST['record_id'];							//赋值记录id
+	$content=dnspod_freeset_srv_remove($domain,$play_domain,$port,$srv_domain,$record_id,$login_token);
 															//连接api 获取返回数组
-if($content['status']['code']!=1)
-{
-	$res=array('errno'=>'4','errmsg'=>$content['status']['message']);
-	echo json_encode($res);									//错误信息输出
-	return;													//结束
-}
-$res=array(
-'errno' =>'1',
-'errmsg'=>iconv("gb2312","UTF-8",'修改成功,新域名为:'.$srv_domain.'.'.$domain.', 请记住新ID,修改和删除域名需要'),
-'record_id'    =>$content['record']['record_id'],
-);															//成功,信息
-echo json_encode($res);										//输出
+	if($content['status']['code']!=1)
+	{
+		$res=array('errno'=>'4','errmsg'=>$content['status']['message']);
+		echo json_encode($res);									//错误信息输出
+		return;													//结束
+	}
+	
+	$res=array(
+		'errno' =>'1',
+		'errmsg'=>iconv("gb2312","UTF-8",'修改成功,新域名为:'.$srv_domain.'.'.$domain.', 请记住新ID,修改和删除域名需要'),
+		'record_id'    =>$content['record']['record_id'],
+	);															//成功,信息
+	echo json_encode($res);										//输出
 }
 
 
